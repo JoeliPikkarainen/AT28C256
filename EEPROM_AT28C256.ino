d99e9f00728bbd979f77cee9155b9996d1f7f82d
H8897 <h8897@student.jamk.fi>
2020-06-08 20:14:03 +0300

    Save working version
    Fixed a bug from wrongly configured ADDRS
    Updated for README

--- EEPROM_AT28C256.ino
+++ EEPROM_AT28C256.ino
@@ -7,29 +7,70 @@
 #include <avr/io.h>
 
 //For setting The 15 ADDRESS pins on the EEPROM
-const uint8_t ADDR[] = {20,22,24,26,28,30,32,34,36,38,40,42,44,46,48};
+const uint8_t ADDR[] = {22,24,26,28,30,32,34,36,38,40,42,44,46,48,50};
 #define NUM_ADDRS 15
 
 //For data I/O on the EEPROM
-const uint8_t DATA[] = {31,33,35,37,39,41,43,47};
+const uint8_t DATA[] = {31,33,35,37,39,41,43,45};
 #define NUM_DATAS 8
 
-//WED Write Enable on the EEPROM
 #define EEPROM_SIZE (32 * 1000)
-#define WED 5
-#define OED 2
-#define CED 4
+#define WED 5   //Write Enable
+#define OED 2   //Output Enable
+#define CED 4   //Chip Enable
 #define BIT_AT(D,A) ((D >> A) & 0x01)
 #define DELAY_MS 0
+#define DELAY_MICRO 0
+
+void enableChip()   {digitalWrite(CED,0);}
+void disableChip()  {digitalWrite(CED,1);}
+void enableOutput() {digitalWrite(OED,0);}
+void disableOutput(){digitalWrite(OED,1);}
+void enableWrite()  {digitalWrite(WED,0);}
+void disableWrite() {digitalWrite(WED,1);}
 
 void setup() {
 
   Serial.begin(115200);
+  
+   for(int i; i < NUM_ADDRS;i++){
+      pinMode(ADDR[i],OUTPUT);
+      }
+   pinMode(WED,OUTPUT);
+   pinMode(OED,OUTPUT);
+   pinMode(CED,OUTPUT);
+   digitalWrite(OED,1);
+   digitalWrite(CED,1);
+   digitalWrite(WED,1);
 }
 
 void loop() {
 
-  write_read_test(0xFF);
+#define TEST_BLOCKS
+//#define TEST_BYTES
+//#define TEST_READ_ALL
+
+#ifdef TEST_BLOCKS
+    uint8_t data_in[8] = { 0x00, 0x01,0x02 ,0x03,0x04,0x05,0x06,0x07};
+    //uint8_t data_in[8] = { 0x00, 0x01,0x00 ,0x00,0x00,0x00,0x00,0x00};
+
+    write_test(8,data_in);
+    Serial.println("-------");
+    uint8_t * data_out;
+    
+    read_test(8,data_out);
+    Serial.println("-------");
+
+    delay(1000);
+#endif
+
+#ifdef TEST_BYTES
+  write_read_test(0x00);
+#endif
+
+#ifdef TEST_READ_ALL
+  read_all();
+#endif
 }
 
 void write_read_test(uint8_t data_in){
@@ -43,124 +84,203 @@ void write_read_test(uint8_t data_in){
 
     data_out = 0;
 
-    char str_address[30];
-    sprintf(str_address,"Address: 0x%04X ",i);
-    Serial.print(str_address);
-    Serial.print("W -> ");
-    Serial.print(data_in);  
+    char str_out[30];
+    sprintf(str_out,"Address: 0x%04X ",i);
+    Serial.print(str_out);
+
+    sprintf(str_out,"W -> 0x%02X",data_in);
+    Serial.print(str_out);   
     write_byte(data_in,i);
 
     read_byte(data_out,i);
-    Serial.print(" R <- ");
-    Serial.println(data_out);
-
-    delay(0);
+    sprintf(str_out," R <- 0x%02X",data_out);
+    Serial.print(str_out);
+    Serial.println();
+    delay(100);
+    data_in++;
     }
 
     
 }
 
-void write_byte(uint8_t data, uint16_t addr){
+void write_all(){
+    char str_out[30];
+    uint8_t data_in = 0;
 
-    //Set the write ADDRESS
+  for(int i = 0; i < EEPROM_SIZE;i++){
+    write_byte(i,i);
+    sprintf(str_out,"WRITE ALL: 0x%04X 0x%02X ",i,data_in);
+    Serial.println(str_out);
+    data_in++;
+    } 
+  }
+void read_all(){
+  char str_out[30];
 
-    //Set for output
-    for(int i; i < NUM_ADDRS;i++){
-      pinMode(ADDR[i],OUTPUT);
-      }
+  for(int i = 0; i < EEPROM_SIZE;i++){
+    uint8_t data_out;
+    read_byte(data_out,i);
+    sprintf(str_out,"READ ALL: 0x%04X 0x%02X ",i,data_out);
+    Serial.println(str_out);
+    }
+      
+}
+void write_test(int num_bytes,uint8_t* data){
 
-    //Set the write address on the parallel I/O
-    for(int i = 0; i < NUM_ADDRS;i++){
-        bool thebit = BIT_AT(addr,i);
-        digitalWrite(ADDR[i], thebit);
-      }
+  char str_out[30];
+  uint16_t addr = 0;
+  for(int i = 0; i < num_bytes;i++){
+    
+    sprintf(str_out,"Write test in: 0x%04X 0x%02X ",addr,data[i]);
+    Serial.print(str_out);
+    Serial.println();
 
-    //Set the write DATA
-    //Set for output
-    for(int i; i < NUM_DATAS;i++){
-        pinMode(DATA[i],OUTPUT);
-      }
+    write_byte(data[i],addr);
+    delay(1000);
+    addr++;
+    }
+}
 
-    //Set the write data on the parallel I/O
-     for(int i = 0; i < NUM_DATAS;i++){
-        bool thebit = BIT_AT(data,i);
-        digitalWrite(DATA[i], thebit);
-      }
-
-   delay(DELAY_MS);
-   EEPROM_put_addr();
-   delay(DELAY_MS);
-   EEPROM_SET_I();
-   delay(DELAY_MS);
+void read_test(int num_bytes,uint8_t* data){
   
+  char str_out[30];
+  uint8_t data_out1;
+  uint8_t data_out2;
+  uint16_t addr = 0;
+
+  for(int i = 0; i < num_bytes;i++){
+    
+    data_out1 = 0;
+    data_out2 = 0;
+    
+    read_byte(data_out1,addr);
+    delay(10);
+    read_byte(data_out2,addr);
+    
+    sprintf(str_out,"Read test out: 0x%04X 0x%02X vs 0x%02X ",addr,data_out1, data_out2);
+    Serial.print(str_out);
+    Serial.println();
+    delay(1000);
+    addr++;
+    
+    }
+}
+
+void write_byte(uint8_t data, uint16_t addr){
+  
+    disableOutput();
+    disableWrite();
+    
+    //Set for output
+    set_data_direction(OUTPUT); 
+    //Set the write ADDRESS
+    set_address(addr);
+    
+    //Set the write data on the parallel I/O
+    set_data(data);
+
+
+   enableChip();
+   delayMicroseconds(DELAY_MICRO);
+   
+   enableWrite();
+   delayMicroseconds(DELAY_MICRO);
+   
+   disableWrite();
+   delayMicroseconds(DELAY_MICRO);
+
+   wait_write_finished(data);
+   set_address(0);
+   disableChip();
 }
 
 
 void read_byte(uint8_t &data, uint16_t addr){
-  
-     //Set the read ADDRESS
 
-    //Set for output
-    for(int i; i < NUM_ADDRS;i++){
-      pinMode(ADDR[i],OUTPUT);
-      }
+    //set the read address
+    set_address(addr);
+    
+    //Set for input read
+    set_data_direction(INPUT);
+    
+    disableOutput();
+    disableWrite();
+    disableChip();
+    delayMicroseconds(100);
 
-    //Set the write address on the parallel I/O
-    for(int i = 0; i < NUM_ADDRS;i++){
-        bool thebit = BIT_AT(addr,i);
-        digitalWrite(ADDR[i], thebit);
-      }
+    enableChip();
+    //delayMicroseconds(DELAY_MICRO);
 
-    //Set the read DATA
-    //Set for input
-    for(int i; i < NUM_ADDRS;i++){
-        pinMode(DATA[i],INPUT);
-      }
-
-    //Set the get data on the parallel I/O
-     for(int i = 0; i < NUM_DATAS;i++){
-        bool thebit = BIT_AT(data,i);
-        digitalWrite(DATA[i], thebit);
-      }
-      
-    EEPROM_SET_O();
-    delay(DELAY_MS);
+    enableOutput();   
+    //delayMicroseconds(DELAY_MICRO);
     
     //Read the I/O pins
     data = 0;
-    for(int i = 0; i < NUM_DATAS;i++){
+    read_io(data);
+
+    disableOutput();
+    disableChip();
+
+}
+
+void set_data_direction(uint8_t mode){
+  
+  for(int i = 0; i < NUM_DATAS;i++){
+    pinMode(DATA[i],mode);
+    }
+}
+
+void set_address(uint16_t addr){
+  
+   for(int i = 0; i < NUM_ADDRS;i++){
+       bool thebit = BIT_AT(addr,i);
+       digitalWrite(ADDR[i], thebit);
+      }
+}
+
+void set_data(uint8_t data){
+  
+   for(int i = 0; i < NUM_DATAS;i++){
+      bool thebit = BIT_AT(data,i);
+      digitalWrite(DATA[i], thebit);
+    }
+  
+}
+
+void read_io(uint8_t &data){
+  
+   for(int i = 0; i < NUM_DATAS;i++){
       bool thebit = digitalRead(DATA[i]);
       data = data | (thebit <<i);
+    }
+    
+}
+
+void wait_write_finished(uint8_t written){
+
+ uint8_t first = 0;
+ uint8_t second = 0;
+ set_data_direction(INPUT);
+  do
+  {
+    enableOutput();
+    delayMicroseconds(DELAY_MICRO);
+    read_io(first);
+    disableOutput();
+    enableOutput();
+    delayMicroseconds(DELAY_MICRO);
+    read_io(second);
+    disableOutput();
+    Serial.println(first);
+    Serial.println(second);
+    Serial.println(written);
+    Serial.println();
+    
+    if((first == second)  || (first == written)){
+        break;
       }
-
-/*
-     Serial.print("R ADDRESS:");
-     Serial.print(addr);
-
-     Serial.print(" R DATA:");
-     Serial.print(data);
-     */
-}
-
-void set_output_enable(){
-  digitalWrite(WED,0);
-}
-
-void EEPROM_SET_I(){
-  //Latch data, start write cycle  
-  digitalWrite(WED,1);
-  digitalWrite(OED,1);
-}
-
-void EEPROM_put_addr(){
-  //Latch address
-  digitalWrite(WED,0);
-  digitalWrite(CED,0);
-}
-
-void EEPROM_SET_O(){
-  //Asserts data from the address to I/O pins
-  digitalWrite(CED,0);
-  digitalWrite(OED,0);
-  digitalWrite(WED,1);  
+    first = 0;
+    second = 0;
+  }while(1);
+  disableChip();
 }
--- README.md
+++ README.md
@@ -2,46 +2,57 @@
 This is project to program AT28C256 EEPROM with ATmega 2560
 
 DONE: Working write and read functions
+
 DONE: Working test function to fill EEPROM with single value and verify
 
+DONE: Fixed a bug caused by wrong ADDRESS pin setup, write, read works now
+
 TODO: Add possibility send .hex file from Serial Monitor to Arduino and program the eeprom
-TODO: Make COM_IF for Arduino, needed functions: set_start_address(uint16_t address), verify(char* hexfile, uint16_t start_address)
+
+TODO: Make COM_IF for Arduino, needed functions: 
+
+void set_start_address(uint16_t address)
+
+bool program(char * hexfile,bool verify)
+
+bool verify(char* hexfile, uint16_t start_address)
+
 
 ## Connections Arduino to AT28C256
 
 ### Address
 
-20    -   A0
+22    -   A0
 
-22    -   A1
+24    -   A1
 
-24    -   A2
+26    -   A2
 
-26    -   A3
+28    -   A3
 
-28    -   A4
+30    -   A4
 
-30    -   A5
+32    -   A5
 
-32    -   A6
+34    -   A6
 
-34    -   A7
+36    -   A7
 
-36    -   A8
+38    -   A8
 
-38    -   A9
+40    -   A9
 
-40    -   A10
+42    -   A10
 
-42    -   A11
+44    -   A11
 
-43    -   A12
+46    -   A12
 
-44    -   A13
+48    -   A13
 
-44    -   A14
+50    -   A14
 
-46    -   A15
+52    -   NC
 
 48    -   NC
 
